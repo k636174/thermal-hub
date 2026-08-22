c72894d855a5b1a8c3dc32b40456c5b6df635a8b
Shinji Kaneko <shinji@dmesg.jp>
2026-08-22 14:54:25 +0900

    Add selectable paper length modes

--- templates/PrintJobs/index.php
+++ templates/PrintJobs/index.php
@@ -4,12 +4,35 @@
 <?php if (!$printers) :
     ?><p>印字するには先に<?= $this->Html->link('プリンターを登録', ['controller' => 'Printers', 'action' => 'add']) ?>してください。</p><?php
 endif; ?>
-<table><thead><tr><th>タイトル</th><th>更新日時</th><th>印字</th><th>操作</th></tr></thead><tbody>
+<table>
+<thead><tr><th>タイトル</th><th>更新日時</th><th>印字</th><th>操作</th></tr></thead>
+<tbody>
 <?php foreach ($jobs as $job) :
     ?><tr><td><?= h($job->title) ?></td><td><?= h($job->modified) ?></td><td>
     <?php if ($printers) :
-        ?><?= $this->Form->create(null, ['url' => ['action' => 'printNow', $job->id]]) ?><?= $this->Form->select('printer_id', $printers, ['required' => true]) ?><?= $this->Form->button('印字', ['confirm' => 'この内容を印字しますか？']) ?><?= $this->Form->end() ?><?php
+        ?>
+        <?= $this->Form->create(null, ['url' => ['action' => 'printNow', $job->id]]) ?>
+        <?= $this->Form->select('printer_id', $printers, ['required' => true]) ?>
+        <?= $this->Form->control('paper_length', [
+            'type' => 'select',
+            'label' => '用紙の長さ',
+            'options' => [
+                'none' => '指定なし',
+                'narrow' => 'システム手帳 ナロー（17cm）',
+                'm5' => 'M5／マイクロ5（10.5cm）',
+            ],
+        ]) ?>
+        <?= $this->Form->button('印字', ['confirm' => 'この内容を印字しますか？']) ?>
+        <?= $this->Form->end() ?>
+        <?php
     endif; ?>
-</td><td><?= $this->Html->link('編集', ['action' => 'edit', $job->id]) ?> <?= $this->Form->postLink('削除', ['action' => 'delete', $job->id], ['confirm' => '削除しますか？']) ?></td></tr><?php
+</td><td>
+    <?= $this->Html->link('編集', ['action' => 'edit', $job->id]) ?>
+    <?= $this->Form->postLink(
+        '削除',
+        ['action' => 'delete', $job->id],
+        ['confirm' => '削除しますか？'],
+    ) ?>
+</td></tr><?php
 endforeach; ?>
 </tbody></table></div>
